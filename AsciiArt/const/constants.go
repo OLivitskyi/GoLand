cfeb1e407d248f51f20deef2334b99edbe910415
oleg_livitskyi <olivitskyi@gmail.com>
2023-12-07 10:09:07 +0200

    work with structure

---
+++ AsciiArt/const/constants.go
@@ -0,0 +1,15 @@
+package _const
+
+const (
+	ResourcesPath           string = "resources/"
+	DefaultBannerName              = "standard"
+	ArgInputText                   = 1
+	ArgBannerName                  = 2
+	AsciiSpace              rune   = 32
+	AsciiTilde              rune   = 126
+	AsciiSymbolsCount       int    = 95
+	WithoutSpacingSeparator        = 9
+	WithSpacingSeparator           = 10
+	RowCountPerChar                = 6
+	AsciiArtStartOffset            = 1
+)
--- AsciiArt/main.go
+++ AsciiArt/main.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/OLivitskyi/AsciiArt/const"
 	"os"
 	"strings"
 )
 
-const (
-	resourcesPath           string = "resources/"
-	defaultBannerName              = "standard"
-	argInputText                   = 1
-	argBannerName                  = 2
-	asciiSpace              rune   = 32
-	asciiTilde              rune   = 126
-	asciiSymbolsCount       int    = 95
-	withoutSpacingSeparator        = 9
-	withSpacingSeparator           = 10
-	rowCountPerChar                = 6
-	asciiArtStartOffset            = 1
-)
-
 func main() {
-	text := os.Args[argInputText]
-	bannerFilename := defaultBannerName + ".txt"
+	text := os.Args[_const.ArgInputText]
+	bannerFilename := _const.DefaultBannerName + ".txt"
 
-	if len(os.Args) > argBannerName {
-		requestedBanner := os.Args[argBannerName]
+	if len(os.Args) > _const.ArgBannerName {
+		requestedBanner := os.Args[_const.ArgBannerName]
 		availableBanners := map[string]bool{
 			"standard":   true,
 			"shadow":     true,
@@ -34,11 +21,11 @@ func main() {
 		if availableBanners[requestedBanner] {
 			bannerFilename = requestedBanner + ".txt"
 		} else {
-			fmt.Println("The Banner", requestedBanner, "is not available. Using", defaultBannerName+".txt", "as a default ASCII Art.")
+			fmt.Println("The Banner", requestedBanner, "is not available. Using", _const.DefaultBannerName+".txt", "as a default ASCII Art.")
 		}
 	}
 
-	asciiArt := convertToAsciiArt(text, resourcesPath+bannerFilename)
+	asciiArt := convertToAsciiArt(text, _const.ResourcesPath+bannerFilename)
 	fmt.Println(asciiArt)
 }
 
@@ -57,18 +44,18 @@ func createMap(filename string) map[rune][]string {
 	}
 
 	bannerLines := strings.Split(string(fileContent), "\n")
-	asciiMap := make(map[rune][]string, asciiSymbolsCount)
+	asciiMap := make(map[rune][]string, _const.AsciiSymbolsCount)
 
 	totalLines := len(bannerLines)
 
-	linesPerSymbol := withoutSpacingSeparator
-	if (totalLines / asciiSymbolsCount) == withSpacingSeparator {
-		linesPerSymbol = withSpacingSeparator
+	linesPerSymbol := _const.WithoutSpacingSeparator
+	if (totalLines / _const.AsciiSymbolsCount) == _const.WithSpacingSeparator {
+		linesPerSymbol = _const.WithSpacingSeparator
 	}
 
-	for char := asciiSpace; char <= asciiTilde; char++ {
-		startLine := asciiArtStartOffset + (int(char)-int(asciiSpace))*linesPerSymbol
-		endLine := startLine + rowCountPerChar
+	for char := _const.AsciiSpace; char <= _const.AsciiTilde; char++ {
+		startLine := _const.AsciiArtStartOffset + (int(char)-int(_const.AsciiSpace))*linesPerSymbol
+		endLine := startLine + _const.RowCountPerChar
 		fullArt := bannerLines[startLine:endLine]
 		asciiMap[char] = fullArt
 	}
@@ -83,7 +70,7 @@ func printArt(lines []string, asciiMap map[rune][]string) string {
 			result += "\n"
 			continue
 		}
-		for row := 0; row < rowCountPerChar; row++ {
+		for row := 0; row < _const.RowCountPerChar; row++ {
 			for _, ch := range word {
 				result += asciiMap[ch][row]
 			}
